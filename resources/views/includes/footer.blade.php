6f5a05cb41b57158f9398523ad29976e67fad983
hw355 <hw355@njit.edu>
2019-03-05 23:35:59 -0500

    Feature: A 2-page website.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,5 @@
 <div class="row mt-4 mb-4"  class="font-weight-light">
-    &copy; 2017-2018 Learana, Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+
+    &copy; 2019 IS601 &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+
 </div>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.default')
+
 @section('content')
 
     <h1>Hello World Welcome to My About Page</h1>
